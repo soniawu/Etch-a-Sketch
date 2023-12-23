3519764e882a5549f7f0a5af1b6e4ca9e3bca25f
soniawu <sonia.wu.0906@gmail.com>
2023-12-22 21:27:33 -0600

    Complete Version 2
    - Add user input for number of per side, 1-100.
    - Draw grids base on user entered number.

--- js/eas.js
+++ js/eas.js
@@ -1,36 +1,65 @@
-let numPerRowCol = 16;
-let gridSize = 30;
+const submit = document.querySelector("#sbmBtn");
+let numPerSide = 0;
+submit.addEventListener("click", () => {
+    numPerSide = (document.querySelector("#numPerSide")).value;
+    cleanUpBoard();
+    gridBoard();
+});
 
-const container = document.querySelector("#container");
-const grids = document.createElement("div");
+/*********************************************/
+function gridBoard() {
 
-grids.id = "grids";
+    const container = document.querySelector("#container");
+    const grids = document.createElement("div");
 
-for (let i=1; i<=numPerRowCol; i++) {
-    // create row div
-    let row = document.createElement("div");
-    let theRow = "row-" + i.toString();
-    row.id = theRow;
-    row.class = "row";
-    
-    for (let j=1; j<=numPerRowCol; j++) {
-        let grid = document.createElement("div");
-        let theGrid = "grid-" + i.toString() + "-" + j.toString();
-        grid.id = theGrid;
-        grid.class = "grid";
-        //grid.textContent = "*";
-        grid.style.border = "0.001em solid gray";
-        grid.style.width = "20px"
-        grid.style.height = "20px"
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "red"
-        })
-        row.appendChild(grid);
+    grids.style.width = "960px";
+    grids.style.height = "960px";
+    grids.style.border = "0.001em solid gray";
+    grids.id = "grids";
+
+    for (let i=1; i<=numPerSide; i++) {
+        // create row div
+        let row = document.createElement("div");
+        let theRow = "row-" + i.toString();
+        row.id = theRow;
+        row.classList.add("row");
+        
+        for (let j=1; j<=numPerSide; j++) {
+            // create grids on a row
+            let grid = document.createElement("div");
+            let gridID = "grid-" + i.toString() + "-" + j.toString();
+            grid.id = gridID;
+            grid.classList.add("grid");
+            grid.style.border = "0.001em solid gray";
+            grid.style.width = "100%"
+
+            // add event listener for the grid, change color when mouse over
+            grid.addEventListener("mouseover", () => {
+                grid.style.backgroundColor = "#c499c0"
+            })
+            
+            row.appendChild(grid);
+        }
+        
+        row.style.display = "flex";
+        row.style.flexDirection = "row";
+        row.style.flexGrow = "1";
+        grids.appendChild(row); 
+        
     }
 
-    row.style.display = "flex";
-    row.style.flexDirection = "row";
-    grids.appendChild(row); 
+    grids.style.display = "flex";
+    grids.style.flexDirection = "column";
+
+    container.appendChild(grids);
+    document.body.appendChild(container);
 }
 
-container.appendChild(grids);
+/**********************************************/
+function cleanUpBoard() {
+    const container = document.querySelector("#container");
+    const grids = document.querySelector("#grids");
+    if (grids) {
+        container.removeChild(grids);
+    }
+}
